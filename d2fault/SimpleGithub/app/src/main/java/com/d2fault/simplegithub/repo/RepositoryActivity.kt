7cef70b606be3090b8ef1f0a1944b93b59df300a
d2fault <d2fault@naver.com>
2020-03-21 14:34:22 +0900

    initial commit

---
+++ d2fault/SimpleGithub/app/src/main/java/com/d2fault/simplegithub/repo/RepositoryActivity.kt
@@ -0,0 +1,13 @@
+package com.d2fault.simplegithub.repo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.d2fault.simplegithub.R
+
+class RepositoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository)
+    }
+}
---
+++ d2fault/SimpleGithub/app/src/main/java/com/d2fault/simplegithub/search/SearchActivity.kt
@@ -0,0 +1,13 @@
+package com.d2fault.simplegithub.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.d2fault.simplegithub.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
