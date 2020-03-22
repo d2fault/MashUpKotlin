c6046c4f09f30378b3d4f5215a41bc0ee84d65a6
d2fault <d2fault@naver.com>
2020-03-22 19:01:49 +0900

    toolbar에 item 추가(search)

--- d2fault/SimpleGithub/app/src/main/java/com/d2fault/simplegithub/ui/search/SearchActivity.kt
+++ d2fault/SimpleGithub/app/src/main/java/com/d2fault/simplegithub/ui/search/SearchActivity.kt
@@ -2,12 +2,17 @@ package com.d2fault.simplegithub.ui.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.d2fault.simplegithub.R
 
 class SearchActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_activity_search, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
